271118abd94b1e796db9416201b35be50882dcf6
amomentforever0 <amomentforever0@gmail.com>
2019-10-14 20:03:54 +0200

    tooltip simple implementation

---
+++ Tooltip/app.js
@@ -0,0 +1,75 @@
+class Tooltip {
+    constructor(config) {
+        this.config = config;
+        this.isHovered = false;
+        this.init();
+    }
+
+    init() {
+        document.body.addEventListener('mouseover', (event) => {
+            if(event.target.classList.contains('s-tooltip')) {
+                this.isHovered = true;
+                this.addTooltip(event);
+            } 
+
+            if(event.target.classList.contains('tooltip')) {
+                this.isHovered = true;
+            }
+            return;
+        });
+
+        document.body.addEventListener('mouseout', (event) => {
+            if(event.target.classList.contains('s-tooltip')) {
+                this.removeTooltip(event);
+            }
+            if(event.target.classList.contains('tooltip')) {
+                this.isHovered = false;
+                this.removeTooltip(event);
+            }
+            return;
+        });
+    }
+
+    addTooltip(event) {
+        const el = event.target;
+        const position = el.dataset.position;
+        const content = el.dataset.content;
+
+        const tooltipPosition = {};
+
+        if(document.querySelector('.tooltip')) {
+            document.querySelector('.tooltip').remove();
+        }
+
+        const tooltip = document.createElement('div');
+        tooltip.classList.add('tooltip');
+        tooltip.textContent = content || this.config.defaultContent;
+        document.body.append(tooltip);
+
+        const coords = el.getBoundingClientRect();
+        let left, top;
+
+        left = coords.left + (el.offsetWidth - tooltip.offsetWidth)/2;
+        top = coords.top - tooltip.offsetHeight - 5;
+
+        tooltip.style.left = left + 'px';
+        tooltip.style.top = top + 'px';
+      };
+
+
+    removeTooltip(event) {
+        this.isHovered = false;
+        setTimeout(() => {
+            if(document.querySelector('.tooltip') && !this.isHovered) {
+                document.querySelector('.tooltip').remove();
+            }
+        }, this.config.hideDelay);
+    }
+}
+
+const tooltip = new Tooltip({
+    showDelay: 200, 
+    hideDelay: 300,
+    defaultContent: 'Hello, tooltip!',
+    defaultPosition: 'top, left'
+});
